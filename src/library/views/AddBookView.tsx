11cf1f9ea99597bd0ba7be31e32ac5db38cd6cfb
Andree Uuetoa <andree.uuetoa@gmail.com>
2023-12-04 21:22:39 +0200

    Make the book form nicer

--- src/library/views/AddBookView.tsx
+++ src/library/views/AddBookView.tsx
@@ -3,42 +3,46 @@ import {IPostBooksProps} from "../domain/IPostBooksProps";
 import {FormPart} from "../../common/viewparts/FormPart";
 
 export const AddBookView = (props: IPostBooksProps) => {
-    const eventHandler = (e: React.ChangeEvent) => {props.handleChange(e.target as EventTarget & HTMLInputElement)};
+    const eventHandler = (e: React.ChangeEvent) => {
+        props.handleChange(e.target as EventTarget & HTMLInputElement)
+    };
 
     const [bookName, setBookName] = useState("");
 
     return (
-        <form id="add-book-form" method="post">
-            <h2>Lisa raamat.</h2>
-            <hr/>
+        <div className="w-50">
+            <form id="add-book-form" method="post">
+                <h2>Lisa raamat.</h2>
+                <hr/>
 
-            <ul style={{'display': props.validationErrors.length === 0 ? 'none' : ''}}>
-                <li>
-                    {props.validationErrors.length > 0 ? props.validationErrors[0] : ''}
-                </li>
-            </ul>
+                <ul style={{'display': props.validationErrors.length === 0 ? 'none' : ''}}>
+                    <li>
+                        {props.validationErrors.length > 0 ? props.validationErrors[0] : ''}
+                    </li>
+                </ul>
 
-            <FormPart fieldElements={{
-                value: props.bookData.name,
-                type: "book-name",
-                placeholder: "Nimi",
-                autocomplete: "book-name",
-                name: "book-name",
-                onchange: eventHandler
-            }} />
-            <FormPart fieldElements={{
-                value: props.bookData.authors,
-                type: "authors",
-                placeholder: "Autorid",
-                autocomplete: "authors",
-                name: "authors",
-                onchange: eventHandler
-            }} />
-            <button
-                onClick={(e) => props.onSubmit(e)}
-                id="registerSubmit" className="w-100 btn btn-lg btn-primary">
-                Register
-            </button>
-        </form>
+                <FormPart fieldElements={{
+                    value: props.bookData.name,
+                    type: "book-name",
+                    placeholder: "Nimi",
+                    autocomplete: "book-name",
+                    name: "book-name",
+                    onchange: eventHandler
+                }}/>
+                <FormPart fieldElements={{
+                    value: props.bookData.authors,
+                    type: "authors",
+                    placeholder: "Autorid",
+                    autocomplete: "authors",
+                    name: "authors",
+                    onchange: eventHandler
+                }}/>
+                <button
+                    onClick={(e) => props.onSubmit(e)}
+                    className="w-100 btn btn-lg submit-btn">
+                    Lisa raamat
+                </button>
+            </form>
+        </div>
     );
 }
