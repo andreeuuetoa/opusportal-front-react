54826b93cd9c9c02c3365cd74857b6ffa7def21d
andreeuuetoa <andree.uuetoa@gmail.com>
2023-12-30 16:41:24 +0200

    Nicer books table structure

--- src/library/viewparts/BooksTable.tsx
+++ src/library/viewparts/BooksTable.tsx
@@ -1,26 +1,20 @@
 import {IGetBooksProps} from "../domain/IGetBooksProps";
 import {BooksTableRow} from "./BooksTableRow";
+import {BooksTableHeader} from "./BooksTableHeader";
 
 export const BooksTable = (books: IGetBooksProps) => {
     return (
         <>
             <div className="table-row">
-            <th>
-                <td>
-                    Raamatu nimi
-                </td>
-                <td>
-                    Autorid
-                </td>
-                <td>
-                    Laenutaja
-                </td>
-            </th>
-            <table>
-                {
-                    books.books.map(b => (<tr> <BooksTableRow name={b.name} authors={b.authors} yearReleased={b.yearReleased}/> </tr>))
-                }
-            </table>
+                <table>
+                    <BooksTableHeader />
+                    <tbody>
+                    {
+                        books.books.map(b => (
+                            <tr><BooksTableRow name={b.name} authors={b.authors} yearReleased={b.yearReleased}/></tr>))
+                    }
+                    </tbody>
+                </table>
             </div>
         </>
     );
---
+++ src/library/viewparts/BooksTableHeader.tsx
@@ -0,0 +1,17 @@
+export const BooksTableHeader = () => {
+    return (
+        <thead>
+        <tr>
+            <th>
+                Raamatu nimi
+            </th>
+            <th>
+                Autorid
+            </th>
+            <th>
+                Laenutaja
+            </th>
+        </tr>
+        </thead>
+    );
+}
