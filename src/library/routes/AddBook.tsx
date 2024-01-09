e1ec2e4f3ac146edbbef279bf29b78afac970e54
andreeuuetoa <andree.uuetoa@gmail.com>
2024-01-09 22:14:44 +0200

    Create a new book with API

--- src/library/routes/AddBook.tsx
+++ src/library/routes/AddBook.tsx
@@ -2,14 +2,14 @@ import React, {useContext, useState} from "react";
 import {BookData} from "../DTO/BookData";
 import {AddBookView} from "../views/AddBookView";
 import {JWTContext} from "../../root/Root";
+import {BookAPI} from "../API/BookAPI";
 
 export const AddBook = () => {
     const {JWTResponse, setJWTResponse} = useContext(JWTContext);
 
     const [values, setValues] = useState({
         name: "",
-        authors: "",
-        yearReleased: 0
+        authors: ""
     } as BookData);
 
     const [validationErrors, setValidationErrors] = useState([] as string[]);
@@ -18,8 +18,21 @@ export const AddBook = () => {
         setValues({...values, [target.name]: target.value});
     };
 
-    const onSubmit = async (event: React.MouseEvent) => {
+    const bookAPI = new BookAPI(setJWTResponse!);
 
+    const onSubmit = async (event: React.MouseEvent) => {
+        event.preventDefault();
+
+        if (values.name.length === 0 ||
+            values.authors.length === 0) {
+            setValidationErrors(["Bad input values!"]);
+            return;
+        }
+
+        // Remove errors
+        setValidationErrors([]);
+
+        await bookAPI.create(values);
     };
 
     return (
